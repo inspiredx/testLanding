41102e667fbc98c557971ba8871a547add88cd15
Georgiy Kapustin <84815448+inspiredx@users.noreply.github.com>
2022-07-08 16:38:16 +0300

    1

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var swiper = new Swiper(".mySwiper", {
-    pagination: {
-      el: ".swiper-pagination",
-      type: "progressbar",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
